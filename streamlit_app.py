05bfdd6814cfe85062a7d4b2f2ce9a8d5be98aba
ichirox-88 <farrelevytra4@gmail.com>
2025-04-26 08:19:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("kelompok 4")
+st.title("kalkulator kimia kelompok 4")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
